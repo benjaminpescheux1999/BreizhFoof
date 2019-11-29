2e772dca738f82f5f53cf5d4460406cb437d6ed8
benjaminpescheux1999 <47316003+benjaminpescheux1999@users.noreply.github.com>
2019-11-29 14:17:15 +0100

    Add files via upload

---
+++ define.php
@@ -0,0 +1,13 @@
+<?php 
+define('WEBROOT',str_replace('index.php','',$_SERVER['SCRIPT_NAME']));
+define('ROOT',str_replace('index.php','',$_SERVER['SCRIPT_FILENAME']));
+define('IMAGE','/2018-breizhinc/asset/img/');
+define('SCRIPT','/2018-breizhinc/asset/js/');
+define('SHEET','/2018-breizhinc/asset/');
+
+
+
+//$GLOBAL['pdo']=$pdo->exec('SET NAMES utf8');
+
+
+?>
